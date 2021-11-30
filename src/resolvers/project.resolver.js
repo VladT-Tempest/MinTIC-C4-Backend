096f86e496b777d49c878065363f439a981d848f
moralesdei <moralesdei@protonmail.com>
2021-11-30 18:31:55 -0500

    Adding mutation function to update a project

--- src/resolvers/project.resolver.js
+++ src/resolvers/project.resolver.js
@@ -16,10 +16,26 @@ const leader = async (parent, args, context, info) => {
   return user;
 };
 
+const update_project = async (parent, args, {user, errorMessage}) => {
+  if(!user) {
+    throw new Error(errorMessage);
+  }
+  return Projects.findByIdAndUpdate(args._id,
+    {name: args.input.name || undefined,
+    generalObjective: args.input.generalObjective || undefined,
+    "$push": {"specificObjectives": args.input.specificObjectives || undefined},
+    budget: args.input.budget || undefined
+    }
+  );
+};
+
 export default {
   projectQueries: {
     allProjects,
     project,
+  },
+  projectMutations: {
+    update_project,
   },
   Project: {
     leader,
